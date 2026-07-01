f0d80b468075cae2dc00af0e8928c054047a2c73
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2026-07-01 18:56:56 +0200

    docs: trim redundant comments

--- includes/http/PathUtils.hpp
+++ includes/http/PathUtils.hpp
@@ -6,7 +6,7 @@
 /*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 13:45:29 by gansari           #+#    #+#             */
-/*   Updated: 2026/06/26 13:58:17 by gansari          ###   ########.fr       */
+/*   Updated: 2026/07/01 18:10:36 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@ namespace PathUtils
 {
 	
 	std::string	lexical_normalize(const std::string& p);
-
-	// True if fs_path stays inside root after resolving "." and ".." -> blocks path traversal
-	// An empty root means "can't enforce" -> allowed
 	bool		is_within_root(const std::string& fs_path, const std::string& root);
 }
 
